35142f8d137b965d367f0126bc2c5ca6a062ad27
Kirill Kazak <kazzak.kirill1995@gmail.com>
2022-08-27 18:16:42 +0300

    Thread example

--- app/src/main/java/kazak/kirill/temperature/MainActivity.kt
+++ app/src/main/java/kazak/kirill/temperature/MainActivity.kt
@@ -2,10 +2,51 @@ package kazak.kirill.temperature
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.VIEW_MODEL_STORE_OWNER_KEY
+import kazak.kirill.temperature.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var vb: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        vb = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(vb.root)
+
+        vb.btnDownload.setOnClickListener {
+            loadData()
+        }
+    }
+
+    private fun loadData() {
+        vb.btnDownload.isEnabled = false
+        vb.pb.visibility = View.VISIBLE
+        getLocation{
+            vb.tvLocation.text = it
+            getTemperature {
+                vb.tvTemperature.text = it.toString()
+                vb.btnDownload.isEnabled = true
+                vb.pb.visibility = View.GONE
+            }
+        }
+
+    }
+
+    private fun getLocation(callback: (String) -> Unit) {
+        thread {
+            Thread.sleep(5000)
+            callback.invoke("Moscow")
+        }
+    }
+
+    private fun getTemperature(callback: (Int) -> Unit) {
+        thread{
+            Toast.makeText(this@MainActivity, "Start temperature loading!", Toast.LENGTH_SHORT).show()
+            Thread.sleep(5000)
+            callback.invoke(17)
+        }
     }
 }
