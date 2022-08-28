09cc3072110bafa32310b86233f7558c6b9d3b74
Kirill Kazak <kazzak.kirill1995@gmail.com>
2022-08-28 13:54:25 +0300

    tune coroutines

--- app/src/main/java/kazak/kirill/temperature/MainActivity.kt
+++ app/src/main/java/kazak/kirill/temperature/MainActivity.kt
@@ -1,54 +1,46 @@
 package kazak.kirill.temperature
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import kazak.kirill.temperature.databinding.ActivityMainBinding
-import kotlin.concurrent.thread
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var vb: ActivityMainBinding
+    private val vb by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        vb = ActivityMainBinding.inflate(layoutInflater)
         setContentView(vb.root)
 
         vb.btnDownload.setOnClickListener {
-            loadData()
+            lifecycleScope.launch {
+                loadData()
+            }
         }
     }
 
-    private fun loadData() {
+    private suspend fun loadData() {
         vb.btnDownload.isEnabled = false
         vb.pb.visibility = View.VISIBLE
-        getLocation{
-            vb.tvLocation.text = it
-            getTemperature {
-                vb.tvTemperature.text = it.toString()
-                vb.btnDownload.isEnabled = true
-                vb.pb.visibility = View.GONE
-            }
-        }
-
+        vb.tvLocation.text = getLocation()
+        vb.tvTemperature.text = getTemperature().toString()
+        vb.btnDownload.isEnabled = true
+        vb.pb.visibility = View.GONE
     }
 
-    private fun getLocation(callback: (String) -> Unit) {
-        thread {
-            Thread.sleep(5000)
-            runOnUiThread { callback.invoke("Moscow") }
-        }
+    private suspend fun getLocation(): String {
+        delay(5000)
+        return "Moscow"
     }
 
-    private fun getTemperature(callback: (Int) -> Unit) {
-        thread{
-            runOnUiThread {
-                Toast.makeText(this@MainActivity, "Start temperature loading!", Toast.LENGTH_SHORT).show()
-            }
-            Thread.sleep(5000)
-
-            runOnUiThread { callback.invoke(17) }
-        }
+    private suspend fun getTemperature(): Int {
+        Toast.makeText(this@MainActivity, "Start temperature loading!", Toast.LENGTH_SHORT).show()
+        delay(5000)
+        return 17
     }
+
 }
